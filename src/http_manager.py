3eed01badb330f4045ca2969dc54328ea2e60cb2
Sergei Morozov <moroz051091@yandex.ru>
2024-04-23 16:39:39 +0400

    refactor(http_manager): isolate data requests into a new module

---
+++ src/http_manager.py
@@ -0,0 +1,20 @@
+import logging
+
+import requests
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('HTTPSession')
+
+
+class HTTPSessionManager:
+    def __init__(self, url):
+        self.url = url
+        self.response = None
+
+    def get_data(self):
+        headers = {'Accept-Encoding': 'identity'}
+        self.response = requests.get(self.url, stream=True, headers=headers)
+        logger.info(f"Response status code: {self.response.status_code}")
+
+    def get_raw_response(self):
+        return self.response.raw if self.response else None
